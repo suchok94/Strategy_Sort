321bafd435ebd9ba22d81223bb7a3f1db86e5d25
suchok94 <suchok94@gmail.com>
2025-05-05 14:04:36 +0300

    создал модуль strategies и заголовки классов

---
+++ strategies.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class IStrategy(ABC):
+    pass
+
+
+class BubbleSort(IStrategy):
+    pass
+
+
+class QuickSort(IStrategy):
+    pass
+
+
+class InsertSort(IStrategy):
+    pass
+
