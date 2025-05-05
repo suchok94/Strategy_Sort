c1f2589dc7ae97b4fea76998b597f703c6c15db2
suchok94 <suchok94@gmail.com>
2025-05-05 14:13:16 +0300

    описал класс Context

--- context.py
+++ context.py
@@ -1,5 +1,15 @@
 from strategies import IStrategy
 
 
-class Context():
-    pass
+class Context:
+
+    def __init__(self, strategy= None):
+        self.__strategy = strategy
+
+    def set_strategy(self, strategy: IStrategy):
+        self.__strategy = strategy
+
+    def execute_strategy(self, list: list[int]):
+        sorted_list = self.__strategy.sort(list)
+        return sorted_list
+
