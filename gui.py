ab96edfe5367a20f83905c3dce19928f5a95eeea
suchok94 <suchok94@gmail.com>
2025-05-05 16:31:53 +0300

    дописал метод и текст

--- gui.py
+++ gui.py
@@ -3,7 +3,10 @@ class GUI:
     @staticmethod
     def hello():
         print(f'Это программа для сортировки чисел\n'
-              f'Выберите сортировку цифрами:')
+              f'Выберите сортировку цифрами:'
+              f'"1" - пузырьком'
+              f'"2" - быстрая'
+              f'"3" - вставками')
 
     @staticmethod
     def get_data():
@@ -13,3 +16,7 @@ class GUI:
     @staticmethod
     def result(data):
         print(f'получилось вот такое {data}')
+
+    @staticmethod
+    def get_arr():
+        print('Введите массив: ')
