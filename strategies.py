dd2e3c55aae5ec91a79ec8a3b8fb3e9f1bcb5d98
suchok94 <suchok94@gmail.com>
2025-05-05 14:57:01 +0300

    описал классы в strategies, описал класс сортировки пузырька

--- strategies.py
+++ strategies.py
@@ -2,17 +2,33 @@ from abc import ABC, abstractmethod
 
 
 class IStrategy(ABC):
-    pass
+
+    @abstractmethod
+    @staticmethod
+    def sort(arr):
+        pass
 
 
 class BubbleSort(IStrategy):
-    pass
 
+    @staticmethod
+    def sort(arr):
+        length = len(arr)
+        for i in range(0, length-1, 1):
+            for j in range(i, length - i - 1, 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j+1] = arr[j + 1], arr[j]
+
+        return arr
 
 class QuickSort(IStrategy):
-    pass
+    @staticmethod
+    def sort(self, arr):
+        pass
 
 
 class InsertSort(IStrategy):
-    pass
+    @staticmethod
+    def sort(self, arr):
+        pass
 
