3298576d0422a8a82622662c1aae09ba6f0a91eb
suchok94 <suchok94@gmail.com>
2025-05-06 14:00:05 +0300

    переделал алгоритм

--- application.py
+++ application.py
@@ -1,23 +1,27 @@
 from gui import GUI
 from context import Context
 from check_strategy import CheckerStrategy
+from arrays import Arrays
 
 class Application:
 
     def run(self):
         loop = True
+        GUI.hello()
         while loop == True:
-
-            GUI.hello()
+            GUI.choice_strategy_message()
             choice = GUI.get_data()
             strategy = CheckerStrategy.check(choice)
-            context = Context()
-            context.set_strategy(strategy)
-            GUI.get_arr()
-            arr_numbers = []
-            insert_arr = GUI.get_data()
-            insert_arr = insert_arr.split(sep=',')
-            arr_numbers.extend(insert_arr)
-            data = context.execute_strategy(arr_numbers)
-            GUI.result(data)
-            loop = False
+            if strategy != None:
+                context = Context()
+                context.set_strategy(strategy)
+                GUI.get_arr()
+                insert_arr = Arrays.str_to_int(Arrays.split(GUI.get_data()))
+                data = context.execute_strategy(insert_arr)
+                GUI.result(data)
+                GUI.choice_different_strategy_message()
+
+            else:
+                GUI.incorrect_strategy()
+                loop = False
+
